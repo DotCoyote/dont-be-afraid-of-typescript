bd47adbb57f32268100249dfab6b410db4a35afe
Lars Eichler <larseichler.le@gmail.com>
2020-05-19 20:55:08 +0200

    Adjustments

--- simple-example-final.ts
+++ simple-example-final.ts
@@ -1,14 +1,14 @@
-const inputOne = document.querySelector('#input1') as HTMLInputElement
-const inputTwo = document.querySelector('#input2') as HTMLInputElement
-const resultInput = document.querySelector('#input3') as HTMLInputElement
-const submitButton = document.querySelector('button')
+const inputOne = document.querySelector('#input1') as HTMLInputElement;
+const inputTwo = document.querySelector('#input2') as HTMLInputElement;
+const resultInput = document.querySelector('#input3') as HTMLInputElement;
+const submitButton = document.querySelector('button') as HTMLButtonElement;
 
 function addNumbers(num1: number, num2: number): number {
   return num1 + num2;
 }
 
 submitButton.onclick = (event: MouseEvent) => {
-  event.preventDefault()
+  event.preventDefault();
   const value1 = parseInt(inputOne.value);
   const value2 = parseInt(inputTwo.value);
   
